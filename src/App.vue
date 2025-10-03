ef8c7e62ad1dafb545c8792f6670795e50323ca4
ProblemMaster <63835@gymnasium.ax>
2025-10-03 13:04:59 +0300

    Todo-listan klar

--- src/App.vue
+++ src/App.vue
@@ -13,6 +13,12 @@ function addItem() {
   list.value.push(listItem)
   itemText.value = ''
 }
+
+function radera(listItem) {
+  list.value = list.value.filter((obj) => {
+    return obj !== listItem
+  })
+}
 </script>
 
 <template>
@@ -23,23 +29,54 @@ function addItem() {
   </header>
 
   <main>
-    <h3>Att göra:</h3>
+    <h3 id="todo">Att göra:</h3>
     <ul>
       <template v-for="itm in list" v-bind:key="itm">
-        <li>
+        <li v-if="!itm.done" class="todoItm" @click="itm.done = !itm.done">
           {{ itm.text }}
         </li>
       </template>
     </ul>
-    <h3>Klart!</h3>
+    <h3 id="klert">Klart!</h3>
     <ul>
       <template v-for="itm in list" v-bind:key="itm">
-        <li v-if="itm.done">
-          {{ itm.text }}
+        <li v-if="itm.done" class="todoItm done" @click="itm.done = !itm.done">
+          {{ itm.text }}<span title="Radera" @click="radera(itm)"> x</span>
         </li>
       </template>
     </ul>
   </main>
 </template>
 
-<style scoped></style>
+<style scoped>
+.todoItm {
+  font-weight: bold;
+  padding: 0.5em;
+  list-style-type: none;
+  cursor: pointer;
+}
+.todoItm:nth-child(odd) {
+  background-color: lightblue;
+}
+.todoItm:nth-child(even) {
+  background-color: lightgreen;
+}
+.done {
+  text-decoration: line-through solid black;
+}
+#klart {
+  color: greenyellow;
+}
+#todo {
+  color: red;
+}
+li span {
+  display: inline-block;
+  position: absolute;
+  right: 3em;
+  background-color: red;
+  color: wheat;
+  padding-left: 0.5em;
+  padding-right: 0.5em;
+}
+</style>
