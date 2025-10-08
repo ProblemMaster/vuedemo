da2e2cbece92dc3e806c0e9e410e51107d3eeecf
ProblemMaster <63835@gymnasium.ax>
2025-10-08 11:08:03 +0300

    Justerat home och about view och lagt till en ny vy

--- routing/src/views/AboutView.vue
+++ routing/src/views/AboutView.vue
@@ -1,15 +1,16 @@
+<script setup>
+import { inject } from 'vue'
+
+const views = inject('HomeViews')
+</script>
+
 <template>
   <div class="about">
     <h1>This is an about page</h1>
   </div>
+  <p>
+    HomeViews har visats <b>{{ views }}</b> gånger
+  </p>
 </template>
 
-<style>
-@media (min-width: 1024px) {
-  .about {
-    min-height: 100vh;
-    display: flex;
-    align-items: center;
-  }
-}
-</style>
+<style></style>
