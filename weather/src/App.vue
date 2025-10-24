7b1204ca1ff104aca46670389c71c359d5ac7f7c
ProblemMaster <63835@gymnasium.ax>
2025-10-24 13:57:17 +0300

    burgarmeny med home och locations länkar som fungerar och animation

--- weather/src/App.vue
+++ weather/src/App.vue
@@ -1,8 +1,13 @@
 <script setup>
 import { RouterLink, RouterView } from 'vue-router'
+import BurgerMenu from './components/burgerMenu.vue'
+import { ref } from 'vue'
+
+const menuActive = ref(false)
 </script>
 
 <template>
+  <BurgerMenu @toggle-menu="menuActive = !menuActive" :active="menuActive" />
   <header>
     <nav>
       <RouterLink to="/">Home</RouterLink>
